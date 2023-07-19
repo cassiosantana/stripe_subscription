73bebf4f69c53ebac9d3ff44421ed30763338c5a
Cassio Santana <cassioo.roger@gmail.com>
2023-07-19 18:20:49 -0300

    Initial commit

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,9 @@
+Stripe.api_key = ENV['STRIPE_API_KEY']
+
+StripeEvent.signing_secret = ENV['STRIPE_WEBHOOK_KEY']
+
+StripeEvent.configure do |config|
+  config.subscribe 'checkout.session.completed' do |event|
+		SubscribeService.call(event)	
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'home#index'
+  devise_for :users
+  get 'home/index'
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
