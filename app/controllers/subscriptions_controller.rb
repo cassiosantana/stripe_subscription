e97f8967fd1a189b87f100d3509f805c4c920805
Cassio Santana <cassioo.roger@gmail.com>
2023-07-20 16:13:36 -0300

    fix(subscriptions): fix checkout session creation

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,7 @@ class SubscriptionsController < ApplicationController
 
   def new
     session = Stripe::Checkout::Session.create(
-      payment_method_type: ["card"],
+      payment_method_types: ['card'],
       client_reference_id: current_user.id,
       customer_email: current_user.email,
       subscription_data: {
@@ -11,7 +11,7 @@ class SubscriptionsController < ApplicationController
                   plan: "premium"
                 }]
       },
-      success_url: "http://localhost:3000/donates/thankyou",
+      success_url: "http://localhost:3000",
       cancel_url: "http://localhost:3000"
     )
     @session_id = session.id
