c194ae7243684aad2bef6c2460d52de9401a2353
Cassio Santana <cassioo.roger@gmail.com>
2023-07-20 17:51:37 -0300

    build: Add endpoint for communicating with stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :users
   resources :subscriptions, only: :new
+
+  mount StripeEvent::Engine, at: '/stripe/webhook'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
