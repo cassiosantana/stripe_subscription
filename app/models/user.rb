4ef4799d48818c881f7ffd0fda42c70260744e7a
Cassio Santana <cassioo.roger@gmail.com>
2023-07-21 14:10:08 -0300

    build(user): Change update subscription verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   def subscribed?
-    subscription_id?
+    subscription_id? || expires_at
   end
 end
