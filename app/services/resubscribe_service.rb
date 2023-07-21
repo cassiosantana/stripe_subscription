e52ab4707a8ce54759e730baa990c20fca35f838
Cassio Santana <cassioo.roger@gmail.com>
2023-07-21 14:08:28 -0300

    build(resubscription): Add resubscribe service

---
+++ app/services/resubscribe_service.rb
@@ -0,0 +1,32 @@
+class ResubscribeService
+  def self.call(user)
+    new(user).call
+  end
+
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+  end
+
+  def call
+    subscription = find_subscription
+
+    return false unless subscription
+
+    update_subscription(subscription)
+  end
+
+  private
+
+  def find_subscription
+    Stripe::Subscription.list(customer: user.stripe_id).first
+  end
+
+  def update_subscription(subscription)
+    Stripe::Subscription.update(
+      subscription.id,
+      cancel_at_period_end: false
+    )
+  end
+end
