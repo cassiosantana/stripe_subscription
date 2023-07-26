a45ad221924dd3cb497c56c1dca1a606b4e6baa1
Cassio Santana <cassioo.roger@gmail.com>
2023-07-26 17:45:18 -0300

    Revert "build: Add subscription model"
    This reverts commit 7e15ffee78fb9d9bbe5a30d2b37641e772c29398.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_203251) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_143507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "subscriptions", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "subscription_id"
-    t.datetime "current_period_start"
-    t.datetime "current_period_end"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_subscriptions_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -40,5 +30,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_203251) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "subscriptions", "users"
 end
