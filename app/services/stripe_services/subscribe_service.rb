ae5e8f33f3ff1ba6a520af695615a920e3de791d
Cassio Santana <cassioo.roger@gmail.com>
2023-07-27 00:52:10 -0300

    refactor(service)Refactor Stripe subscription service
    Updated the Stripe subscription service to increase maintainability and readability. Instead of retrieving the user and object data each time a subscription is created, these data are now retrieved when the service class is instantiated. This reduces the number of queries and keeps the code DRY. The subscription creation and update process are also now separated into distinct methods, improving the code's organization and clarity.

--- app/services/stripe_services/subscribe_service.rb
+++ app/services/stripe_services/subscribe_service.rb
@@ -1,22 +1,42 @@
 module StripeServices
   class SubscribeService < BaseService
-    attr_reader :event
+    attr_reader :event, :object, :user, :subscription
 
     def initialize(event)
       @event = event
+      @object = event.data.object
+      @user = User.find_by(id: @object.client_reference_id)
+      @subscription = Stripe::Subscription.retrieve(@object.subscription)
     end
 
     def call
-      user = User.find_by(id: object.client_reference_id)
-      return unless user
+      subscription_record = Subscription.find_by(user_id: user.id, subscription_id: subscription.id)
 
-      user.update(stripe_id: object.customer, subscription_id: object.subscription)
+      if subscription_record
+        update_subscription(subscription_record)
+      else
+        create_subscription
+      end
     end
 
     private
 
-    def object
-      @object ||= event.data.object
+    def update_subscription(subscription_record)
+      subscription_record.update(
+        current_period_end: Time.at(subscription.current_period_end),
+        current_period_start: Time.at(subscription.current_period_start),
+        active: true
+      )
+    end
+
+    def create_subscription
+      Subscription.create(
+        user_id: user.id,
+        subscription_id: subscription.id,
+        current_period_end: Time.at(subscription.current_period_end),
+        current_period_start: Time.at(subscription.current_period_start),
+        active: true
+      )
     end
   end
 end
