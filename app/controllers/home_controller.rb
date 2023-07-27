a06a7b0d59e625bc7a7f068ad261766187504cfb
Cassio Santana <cassioo.roger@gmail.com>
2023-07-27 00:59:28 -0300

    refactor(controller): Refactor subscription check in home controller
    The check was replaced with current_user.subscription.active, ensuring that the user indeed has an active subscription before trying to retrieve it from Stripe.
    
    Ref #16

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    if current_user&.subscription_id
-      subscription = ::Stripe::Subscription.retrieve(current_user.subscription_id)
+    if current_user&.subscription.active
+      subscription = ::Stripe::Subscription.retrieve(current_user.subscription.subscription_id)
       @subscription_plan = subscription.plan.product.capitalize
     end
   end
