6cefd81dc183dd23993711df4153a99926e8657f
Cassio Santana <cassioo.roger@gmail.com>
2023-07-26 23:26:29 -0300

    chore(controller): Update attribute name to better reflect stored value
    The attribute @subscription_id was incorrectly named as it didn't actually store a subscription id, but rather the name of the subscription plan. I renamed it @subscription_plan to make the code more intuitive and easier to maintain in future.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user&.subscription_id
       subscription = ::Stripe::Subscription.retrieve(current_user.subscription_id)
-      @subscription_id = subscription.plan.product.capitalize
+      @subscription_plan = subscription.plan.product.capitalize
     end
   end
 end
