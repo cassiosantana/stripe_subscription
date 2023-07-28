55f5eba6aa78ba401fe461d1e9658caac49133d5
Cassio Santana <cassioo.roger@gmail.com>
2023-07-27 22:43:51 -0300

    refactor: Simplify subscription service
    Ref #13

--- app/services/stripe_services/subscription_service.rb
+++ app/services/stripe_services/subscription_service.rb
@@ -2,17 +2,9 @@ module StripeServices
   class SubscriptionService < BaseService
     private
 
-    def find_subscription_by_customer(customer_id)
-      ::Stripe::Subscription.list(customer: customer_id).first
-    end
-
-    def find_subscription_by_id(subscription_id)
-      ::Stripe::Subscription.retrieve(subscription_id)
-    end
-
-    def update_subscription(subscription, cancel_at_period_end)
+    def update_subscription(subscription_id, cancel_at_period_end)
       ::Stripe::Subscription.update(
-        subscription.id,
+        subscription_id,
         cancel_at_period_end: cancel_at_period_end
       )
     end
@@ -41,7 +33,7 @@ module StripeServices
     end
 
     def item_id
-      find_subscription_by_id(subscription_id).items.data.first.id
+      ::Stripe::Subscription.retrieve(subscription_id).items.data.first.id
     end
   end
 end
