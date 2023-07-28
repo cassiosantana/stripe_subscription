048afa403e2543f345a5ee789d4ecdb20972203e
Cassio Santana <cassioo.roger@gmail.com>
2023-07-27 22:40:43 -0300

    build: Added a one-to-one association between User and Customer models
    Ref #13

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_one :subscription, dependent: :destroy
+  has_one :customer, dependent: :destroy
 end
